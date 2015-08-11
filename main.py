6c426f237df7c3444bc5a4d5259c2e875d05261a
Nathan Wailes <nathan.wailes@gmail.com>
2015-08-10 19:52:28 -0700

    Go to a profile and send them a message

--- main.py
+++ main.py
@@ -29,8 +29,17 @@ for cookie in phantomjs_cookies:
     browser.add_cookie(cookie)
 
 
-# History
-browser.get('https://www.okcupid.com/match')
+browser.get('https://www.okcupid.com/profile/okcupid1865')
+elements = browser.find_elements_by_partial_link_text('Message')
+
+element = elements[1]
+element.click()
+
+message_box = browser.find_element_by_xpath("//textarea[starts-with(@id, 'message_')]")
+message_box.send_keys("Test message")
+
+send_button = browser.find_element_by_xpath("//*[@id='global_messaging_container']/div/form/button")
+send_button.click()
 print("Done!")
 '''
 history_re = r'<td class="item"><a href="(.*)">'
