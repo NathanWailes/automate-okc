ae2777d84d08ba2c6186dd81b8473edd461c961d
Nathan Wailes <nathan.wailes@gmail.com>
2015-08-10 18:54:23 -0700

    log into okc and go to the match page

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ p.add_argument('--avoid-restaurant', default=[], action='append')
 p.add_argument('--place-order', action='store_true')
 p.add_argument('--preserve-browser', action='store_true')
 p.add_argument('--menu-number', default=1)
-p.add_argument('--cookies-path', default='myeatclub_cookies')
+p.add_argument('--cookies-path', default='okc_cookies')
 cfg = p.parse_args(sys.argv[1:])
 
 # Load cookies
@@ -21,15 +21,18 @@ with open(cfg.cookies_path) as c:
     cookies = simplejson.load(c)
     phantomjs_cookies = []
     for key, val in cookies.items():
-        phantomjs_cookies.append({"name": key, "value": val, "domain": "myeatclub.com"})
+        phantomjs_cookies.append({"name": key, "value": val, "domain": "okcupid.com"})
 
 browser = webdriver.Chrome()
-browser.get('https://myeatclub.com')
+browser.get('https://www.okcupid.com')
 for cookie in phantomjs_cookies:
     browser.add_cookie(cookie)
 
+
 # History
-browser.get('https://www.myeatclub.com/orders/history/')
+browser.get('https://www.okcupid.com/match')
+print("Done!")
+'''
 history_re = r'<td class="item"><a href="(.*)">'
 history = set()
 for match in re.findall(history_re, browser.page_source):
@@ -84,3 +87,4 @@ print(result)
 
 if not cfg.preserve_browser:
     browser.quit()
+'''
