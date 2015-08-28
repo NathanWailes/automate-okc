ae5663f48e21069ad327696c416e4d5f6d746dfa
Nathan Wailes <nathan.wailes@gmail.com>
2015-08-28 11:36:58 -0700

    create a working version of notification_that_i_visited.py

--- send/like.py
+++ send/like.py
@@ -4,15 +4,13 @@ from utils import get_config, load_browser
 def main(username):
     cfg = get_config()
     browser = load_browser(cfg)
-
     browser.get('https://www.okcupid.com/profile/' + username)
+
     elements = browser.find_elements_by_xpath('//*[@id="rate_user_profile"]')
-    print(elements)
     like_button = elements[0]
     time.sleep(1)
-    like_button.click()
 
-    print("Done!")
+    like_button.click()
 
 if __name__ == '__main__':
     test_username = 'okcupid1865'
--- send/notification_that_i_visited.py
+++ send/notification_that_i_visited.py
@@ -1,28 +1,17 @@
 import time
 from utils import get_config, load_browser
 
-def main():
+def main(username):
     cfg = get_config()
     browser = load_browser(cfg)
-    #browser.maximize_window()
+    browser.get('https://www.okcupid.com/profile/' + username)
 
-    browser.get('https://www.okcupid.com/profile/okcupid1865')
+    elements = browser.find_elements_by_xpath('//*[@id="visit_button"]/button')
+    like_button = elements[0]
+    time.sleep(1)
 
-
-    ''' Need to update the stuff below
-    elements = browser.find_elements_by_partial_link_text('Message')
-
-    element = elements[1]
-    element.click()
-
-    message_box = browser.find_element_by_xpath("//textarea[starts-with(@id, 'message_')]")
-    message_box.send_keys("Test message")
-
-    send_button = browser.find_element_by_xpath("//*[@id='global_messaging_container']/div/form/button")
-    '''
-
-    send_button.click()
-    print("Done!")
+    like_button.click()
 
 if __name__ == '__main__':
-    main()
+    test_username = 'okcupid1865'
+    main(test_username)
